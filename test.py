29af19d845bee9292784be9c824cbd0298affb64
Dan2754 <dan.978.6a@gmail.com>
2021-11-07 17:59:00 +0300

    add def click_newbtn():     window1.deiconify()     l2 = []     file1 = open('C:/2/data1.txt', 'r')     for line1 in file1:         l2.append(line1)     msg1 = Message(window1, width=500, pady=10)     global k     k += 1     b = "Подсказка " + str(k)     b1 = Label(window1, text=b, font=("Arial Bold", 30))     b1.place(relx=.0, rely=.0)     msg1.config(text=l2[k], font=("times",20))     msg1.place(relx=.5, rely=.6, anchor="c") newbtn = Button(window, text="Подсказка", command=click_newbtn) newbtn.place(relx=.6, rely=.7) lbl5 = lbl5_entry.get()

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ window.geometry('1920x1080')
 #window.attributes('-fullscreen', True) потом включить
 window1 = Tk()
 window1.title("Подсказки")
-window1.geometry('800x800')
+window1.geometry('800x500')
 window1.withdraw()
 
 lbl5 = StringVar()
@@ -34,6 +34,8 @@ name_entry.grid(row=0, column=1, padx=5, pady=5)
 surname_entry.grid(row=1, column=1, padx=5, pady=5)
 
 n=0
+s=0
+k=0
 def click_button():
     name = name_entry.get() # переменной name задается введеное выражение
     surname = surname_entry.get()
@@ -44,21 +46,29 @@ def click_button():
     for line in file:
         l1.append(line)
     msg = Message(window, width=500, pady=10)
-    msg.config(text=l1[5], bg="white", bd=10, relief=GROOVE, font=("times",16, "italic"))
+    msg.config(text=l1[s], bg="white", bd=10, relief=GROOVE, font=("times",20, "italic"))
     msg.place(relx=.5, rely=.1, anchor="c")
     lbl3 = Label(window, text="Задача 1", font=("Arial Bold", 30))
     lbl3.place(relx=.0, rely=.0)
-    l2=[]
-    newButton = Button(window, text="Подсказка", command=lambda:(open('C:/2/data1.txt','r'), open_window1()))
-    for line1 in newButton:
+def click_newbtn():
+    window1.deiconify()
+    l2 = []
+    file1 = open('C:/2/data1.txt', 'r')
+    for line1 in file1:
         l2.append(line1)
-    msg = Message(window1, width=500, pady=10)
-    msg.config(text=l2[5], bg="white", bd=10, relief=GROOVE, font=("times", 16, "italic"))
-    msg.place(relx=.5, rely=.1, anchor="c")
-    newButton.place(relx=.6, rely=.6)
-    lbl5 = lbl5_entry.get()
-btn = Button(root, text="Продолжить",padx="40", pady="40", command=click_button)
-btn.place(relx=.8, rely=.8, anchor="c",width=130, height=30)
+    msg1 = Message(window1, width=500, pady=10)
+    global k
+    k += 1
+    b = "Подсказка " + str(k)
+    b1 = Label(window1, text=b, font=("Arial Bold", 30))
+    b1.place(relx=.0, rely=.0)
+    msg1.config(text=l2[k], font=("times",20))
+    msg1.place(relx=.5, rely=.6, anchor="c")
+newbtn = Button(window, text="Подсказка", command=click_newbtn)
+newbtn.place(relx=.6, rely=.7)
+lbl5 = lbl5_entry.get()
+btn = Button(root, text="Продолжить", padx="40", pady="40", command=click_button)
+btn.place(relx=.8, rely=.8, anchor="c", width=130, height=30)
 def click_btn1():
     global n
     n += 1
@@ -66,14 +76,15 @@ def click_btn1():
 # задача, а номер после каждого клика увеличивается на 1
     lbl2 = Label(window, text=a, font=("Arial Bold", 30))
     lbl2.place(relx=.0, rely=.0)
-    l3=[]
+    l1=[]
     file = open('C:/2/data.txt', 'r')
     for line in file:
-        l3.append(line)
+        l1.append(line)
     msg1 = Message(window, width=500, pady=10)
-    msg1.config(text=l3[4], bg="white", bd=10, relief=GROOVE, font=("times", 16, "italic"))
+    msg1.config(text=l1[s+1], bg="white", bd=10, relief=GROOVE, font=("times", 16, "italic"))
     msg1.pack()
 btn1 = Button(window, text="Далее", padx="40", pady="40", command=click_btn1)
 btn1.place(relx=.8, rely=.8, anchor="c",width=130, height=30)
 root.mainloop()
 window.mainloop()
+window1.mainloop()
